2babb93c871a560a6a13e02302508c8eaa9a40e6
antrzej <antrzej@gmail.com>
2014-08-24 22:25:54 +0200

    assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## "makeCacheMatrix" and "cacheSolve" are functions allowing
+## to store a matrix and calculate its inverse matrix in a way
+## that result will be stored for consecutive uses (won't have to
+## be calculated each time.
 
-## Write a short comment describing this function
+## Defines sets of functions to store the matrix and its inverse matrix
+makeCacheMatrix <- function (x = matrix()){
+	i <- NULL  ## "i" stores inverse matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+	## set matrix stored in scope of given's "makeCacheMatrix"
+	## instance and nullify inverse matrix stored in "i"
+	set <- function (y) {  
+		x <<- y
+		i <<- NULL
+	}
+	## return matrix stored in scope of given's "makeCacheMatrix" instance
+	get <- function() x
 
+	## set inverse matrix stored in scope of given's "makeCacheMatrix" instance
+	setInverse <- function(inverse) i<<-inverse
+	## return inverse matrix stored in scope of given's "makeCacheMatrix" instance
+	getInverse <- function() i
+
+	## return list of functions managing given "makeCacheMatrix" instance
+	list (set=set, get=get,
+		setInverse=setInverse,
+		getInverse=getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## Calculates (only if needed) and returns inverse matrix for the matrix currently 
+## stored in given's "makeCacheMatrix" instance
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	i <- x$getInverse()
+	if(!is.null(i)) return (i) ## if inverse matrix already available, return it
+
+	## otherwise, calculate inverse matrix, set it in the instance of "makeCacheMatrix"
+	## and return it
+	data <- x$get()
+	i <- solve(data, ...)
+	x$setInverse(i)
+	i
 }
